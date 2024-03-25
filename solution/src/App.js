1c4f0d2d7e781467ff1bbbce2f9c32487511dafb
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-24 22:34:19 -0700

    feat: scaffold ui for users form

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,6 +1,21 @@
 function App() {
   return (
     <div className="App">
+      <form>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" />
+
+        <label htmlFor="location">Location</label>
+        <select id="location" name="location">
+          <option value="Canada">Canada</option>
+          <option value="China">China</option>
+          <option value="USA">USA</option>
+          <option value="Brazil">Brazil</option>
+        </select>
+
+        <button type="reset">Clear</button>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
