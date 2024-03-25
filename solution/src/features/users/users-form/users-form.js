4831854f0467d6083f8b3458bfbcb587ff4e65db
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-24 23:20:53 -0700

    feat: validate name on change using api

--- solution/src/features/users/users-form/users-form.js
+++ solution/src/features/users/users-form/users-form.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { UsersDispatchContext } from "../users-context";
-import { getLocations } from "../../../mock-api/apis";
+import { getLocations, isNameValid } from "../../../mock-api/apis";
 
 /**
  * UsersForm component representing a form for adding new users.
@@ -28,6 +28,19 @@ export const UsersForm = () => {
   };
 
   /**
+   * Handles name input change.
+   * @param event
+   * @returns {Promise<void>}
+   */
+  const onNameInputChange = async (event) => {
+    const name = event.target.value;
+    const isValid = await isNameValid(name);
+    isValid
+      ? console.debug("Name is valid:", name)
+      : console.debug("Name is invalid:", name);
+  };
+
+  /**
    * On mount
    */
   useEffect(() => {
@@ -43,7 +56,7 @@ export const UsersForm = () => {
     <form onSubmit={onFormSubmit}>
       {/* Input field to enter user's name */}
       <label htmlFor="name">Name</label>
-      <input id="name" name="name" type="text" required />
+      <input id="name" name="name" type="text" onChange={onNameInputChange} required />
 
       {/* Dropdown to select user's location */}
       <label htmlFor="location">Location</label>
