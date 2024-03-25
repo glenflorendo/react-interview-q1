42b2d44a7827954fc8cb37fcf06f2ed9f00a34d2
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-25 00:31:37 -0700

    style: apply styles to app

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,13 +1,19 @@
 import { UsersProvider } from "./features/users/users-context";
 import { UsersForm } from "./features/users/users-form";
 import { UsersTable } from "./features/users/users-table";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <UsersProvider>
-        <UsersForm />
-        <UsersTable />
+        <div className="section-container">
+          <UsersForm />
+        </div>
+
+        <div className="section-container">
+          <UsersTable />
+        </div>
       </UsersProvider>
     </div>
   );
