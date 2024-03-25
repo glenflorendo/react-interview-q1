5fca77b79738a3044559da7ae602b019b5833095
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2024-03-24 22:41:51 -0700

    feat: scaffold ui for users table

--- solution/src/App.js
+++ solution/src/App.js
@@ -16,6 +16,26 @@ function App() {
         <button type="reset">Clear</button>
         <button type="submit">Add</button>
       </form>
+
+      <table>
+        <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Location</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+          <td>Name 1</td>
+          <td>Location 1</td>
+        </tr>
+        <tr>
+          <td>Name 2</td>
+          <td>Location 2</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
